202c423cbab8e7a9fd01970d35e2164e4e2e7a84
Mairon Vilela <maironvilela@hotmail.com>
2021-11-05 16:18:04 -0300

    add swagger library to documentataion

--- backend/src/shared/infra/http/app.ts
+++ backend/src/shared/infra/http/app.ts
@@ -7,11 +7,14 @@ import express, {
 } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../../../swagger.json';
 import AppError from '../../error/AppError';
 
 
 export const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(express.json());
 app.use(cors());
 
